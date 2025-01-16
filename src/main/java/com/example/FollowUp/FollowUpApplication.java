ff034493b7db72572e99aff594c57c30fc371c97
Holman Rodriguez <holmanandresrodriguezleon55@gmail.com>
2025-01-15 22:00:14 -0500

    First Commit

---
+++ src/main/java/com/example/FollowUp/FollowUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.FollowUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FollowUp/FollowUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FollowUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
